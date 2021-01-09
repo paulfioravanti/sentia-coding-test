a4dbf81e055136f95013348bea7ca841d597e078
Paul Fioravanti <paul.fioravanti@gmail.com>
2021-01-09 17:23:30 +1100

    Initial sorting

--- app/controllers/people_controller.rb
+++ app/controllers/people_controller.rb
@@ -1,7 +1,22 @@
 class PeopleController < ApplicationController
+  helper_method :sort_column, :sort_direction
+
   def index
-    order = params[:order] || :first_name
-    people = Person.includes(:locations, :affiliations).order(order)
+    people =
+      Person
+        .includes(:locations, :affiliations)
+        .order("#{sort_column} #{sort_direction}")
+
     @people = PersonDecorator.decorate_collection(people)
+  end
+
+  private
+
+  def sort_column
+    Person.column_names.include?(params[:sort]) ? params[:sort] : "first_name"
+  end
+
+  def sort_direction
+    %w[asc desc].include?(params[:direction]) ? params[:direction] : "asc"
   end
 end
